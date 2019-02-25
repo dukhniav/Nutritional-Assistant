5e11bc37e077c8512304ee04fa13b0895082ff7e
dukhniav <artem.dukhnitskiy@gmail.com>
2019-02-24 22:52:31 -0800

    made show recipes activity and added helper to implement DataBase... buth currently empty

---
+++ app/src/main/java/com/example/nutritionalassistant/ShowRecipesActivity.kt
@@ -0,0 +1,15 @@
+package com.example.nutritionalassistant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class ShowRecipesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_recipes)
+        
+    }
+
+}
