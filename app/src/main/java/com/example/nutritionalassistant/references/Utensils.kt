b69d63b81b185fb57cc891a3db46af4d42374782
ketterj <ketterj@wwu.edu>
2019-03-12 17:00:46 -0700

    Fully implimented educational reference

--- app/src/main/java/com/example/nutritionalassistant/references/Utensils.kt
+++ app/src/main/java/com/example/nutritionalassistant/references/Utensils.kt
@@ -1,7 +1,6 @@
 package com.example.nutritionalassistant.references
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import com.example.nutritionalassistant.R
 
@@ -12,12 +11,7 @@ class Utensils : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_utensils)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
